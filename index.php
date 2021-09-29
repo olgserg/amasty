f15e99d592cab15a085611028f1afc570266887a
olgserg <91532091+olgserg@users.noreply.github.com>
2021-09-29 12:04:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 
-$colorsList =
-	['red'=>'КРАСНЫЙ', 'blue'=>'ГОЛУБОЙ', 'green'=>'ЗЕЛЕНЫЙ', 'yellow'=>'ЖЕЛТЫЙ',
-	'lime'=>'САЛАТОВЫЙ', 'magenta'=>'ФИОЛЕТОВЫЙ', 'black'=>'ЧЕРНЫЙ',
-	'gold'=>'ЗОЛОТОЙ', 'gray'=>'СЕРЫЙ', 'tomato'=>'ТОМАТНЫЙ'];
+$colorsList = ['red'=>'КРАСНЫЙ', 'blue'=>'СИНИЙ', 'green'=>'ЗЕЛЕНЫЙ', 'yellow'=>'ЖЕЛТЫЙ', 'lime'=>'САЛАТОВЫЙ', 'magenta'=>'ПУРПУРНЫЙ', 'black'=>'ЧЕРНЫЙ', 'gold'=>'ЗОЛОТОЙ', 'gray'=>'СЕРЫЙ', 'tomato'=>'ТОМАТНЫЙ'];
 
 function getColor($color)
 {
 	global $colorsList;
 	$randColor = array_rand($colorsList);
-	if($randColor!=$color){
+	if($randColor != $color){
 		return $randColor;
 	} else getColor($color);
 }
 
 for($i=0; $i<5; $i++)
 {
-	foreach (array_slice($colorsList, $i, 5) as $key=>$item) {
-		echo "<span style='color:" . getColor($key) ."'>$item</span> ";
+	foreach (array_slice($colorsList, $i, 5) as $key => $item) {
+		echo "<span style='color:" . getColor($key) ."'> $item </span> ";
 	}
 	echo "<br>";
 }
